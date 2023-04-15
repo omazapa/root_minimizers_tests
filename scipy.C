06351051e81d1e66db874185bbac0387c5d01660
Omar Zapata <Omar.Zapata@cern.ch>
2023-04-14 20:47:35 -0500

    updated scipy.C tests with bounds

--- scipy.C
+++ scipy.C
@@ -1,5 +1,6 @@
 #include "Math/ScipyMinimizer.h"
 #include "Math/MultiNumGradFunction.h"
+#include <Fit/ParameterSettings.h>
 #include "Math/Functor.h"
 #include <string>
 #include "Math/MinimizerOptions.h"
@@ -46,32 +47,41 @@ int scipy()
    TStopwatch t;
    for(const std::string &text : methods)
    {
-   ROOT::Math::Experimental::ScipyMinimizer minimizer(text.c_str());
-   minimizer.SetMaxFunctionCalls(1000000);
-   minimizer.SetMaxIterations(100000);
-   minimizer.SetTolerance(1e-3);
-   minimizer.SetExtraOption("gtol",1e-3);
-   ROOT::Math::GradFunctor f(&RosenBrock,&RosenBrockGrad,2); 
-   double step[2] = {0.01,0.01};
-   double variable[2] = { -1.2,1.0};
- 
-   minimizer.SetFunction(f);
-   minimizer.SetHessianFunction(RosenBrockHessian);
-   //minimizer.SetFunction(rgf);
-   //minimizer.SetFunction(wgf);
- 
-   // Set the free variables to be minimized!
-   minimizer.SetVariable(0,"x",variable[0], step[0]);
-   minimizer.SetVariable(1,"y",variable[1], step[1]);
-   t.Reset();
-   t.Start();
-   minimizer.Minimize(); 
-   t.Stop();
-   const double *xs = minimizer.X();
-   cout << "Minimum: f(" << xs[0] << "," << xs[1] << "): " 
-        << RosenBrock(xs) << endl;
-   cout << "Cpu Time (sec) = " << t.CpuTime() <<endl<< "Real Time (sec) = " << t.RealTime() << endl;
-   cout << endl << "===============" << endl;
+      ROOT::Math::Experimental::ScipyMinimizer minimizer(text.c_str());
+      minimizer.SetMaxFunctionCalls(1000000);
+      minimizer.SetMaxIterations(100000);
+      minimizer.SetTolerance(1e-3);
+      minimizer.SetExtraOption("gtol",1e-3);
+      ROOT::Math::GradFunctor f(&RosenBrock,&RosenBrockGrad,2); 
+      double step[2] = {0.01,0.01};
+      double variable[2] = { -1.2,1.0};
+   
+      minimizer.SetFunction(f);
+      minimizer.SetHessianFunction(RosenBrockHessian);
+      //minimizer.SetFunction(rgf);
+      //minimizer.SetFunction(wgf);
+   
+      // Set the free variables to be minimized!
+      minimizer.SetLowerLimitedVariable(0,"x",variable[0], step[0],-2);
+      //minimizer.SetVariable(0,"x",variable[0], step[0]);
+      minimizer.SetVariable(1,"y",variable[1], step[1]);
+      //minimizer.SetVariableLimits(0, -2.0, 2.0);
+      //minimizer.SetVariableLowerLimit(0, -10); //BUG WITH THIS
+      minimizer.SetVariableLimits(1, -2.0, 2.0);
+      //ROOT::Fit::ParameterSettings varsettings;
+      //minimizer.GetVariableSettings(0, varsettings);
+      //std::cout<<"HAS LOWER LIMMIT ="<<varsettings.HasLowerLimit()<<std::endl;      
+      //std::cout<<"LOWER LIMMIT ="<<varsettings.LowerLimit()<<std::endl;
+
+      t.Reset();
+      t.Start();
+      minimizer.Minimize(); 
+      t.Stop();
+      const double *xs = minimizer.X();
+      cout << "Minimum: f(" << xs[0] << "," << xs[1] << "): " 
+         << RosenBrock(xs) << endl;
+      cout << "Cpu Time (sec) = " << t.CpuTime() <<endl<< "Real Time (sec) = " << t.RealTime() << endl;
+      cout << endl << "===============" << endl;
    }
    return 0;
 }
